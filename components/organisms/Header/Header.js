8df3ade010ee19412dadae14de61b04d65fdc262
Velrin Black <velrinblack@gmail.com>
2022-08-17 09:05:55 +0200

    Add mobile version of navigation

--- components/organisms/Header/Header.js
+++ components/organisms/Header/Header.js
@@ -1,17 +1,40 @@
 import StyledWrapper from './Header.style';
+import { useState } from 'react';
 
 const Header = ({ aboutRef, offerRef, contactRef }) => {
   const scrollToRef = (ref) => {
     ref.current.scrollIntoView({ behavior: 'smooth' });
   };
 
+  const scrollToRefMobile = (ref) => {
+    setHamburgerActive(false);
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  const [hamburgerActive, setHamburgerActive] = useState(false);
+
   return (
-    <StyledWrapper>
+    <StyledWrapper hamburgerActive={hamburgerActive}>
       <div className='logo'></div>
       <nav className='navigation'>
         <li onClick={() => scrollToRef(aboutRef)}>O nas</li>
         <li onClick={() => scrollToRef(offerRef)}>Oferta</li>
         <li onClick={() => scrollToRef(contactRef)}>Kontakt</li>
+      </nav>
+      <button
+        className='hamburger'
+        onClick={() => {
+          setHamburgerActive((prevState) => !prevState);
+        }}
+      >
+        <div className='stripe'></div>
+        <div className='stripe'></div>
+        <div className='stripe'></div>
+      </button>
+      <nav className='mobile-navigation'>
+        <li onClick={() => scrollToRefMobile(aboutRef)}>O nas</li>
+        <li onClick={() => scrollToRefMobile(offerRef)}>Oferta</li>
+        <li onClick={() => scrollToRefMobile(contactRef)}>Kontakt</li>
       </nav>
     </StyledWrapper>
   );
--- components/organisms/Header/Header.style.js
+++ components/organisms/Header/Header.style.js
@@ -20,6 +20,11 @@ const StyledWrapper = styled.header`
     padding: 0 60px;
   }
 
+  @media (max-width: 1024px) {
+    height: 80px;
+    padding: 0 40px;
+  }
+
   .logo {
     width: 100px;
     height: 40px;
@@ -38,6 +43,10 @@ const StyledWrapper = styled.header`
       width: 280px;
     }
 
+    @media (max-width: 1024px) {
+      display: none;
+    }
+
     li {
       padding: 10px;
 
@@ -50,6 +59,84 @@ const StyledWrapper = styled.header`
       @media (max-width: 2560px) {
         font-size: ${({ theme }) => theme.fontSizes.S};
       }
+    }
+  }
+
+  .hamburger {
+    width: 30px;
+    height: 20px;
+
+    box-sizing: content-box;
+    padding: 10px;
+
+    flex-direction: column;
+    justify-content: space-between;
+
+    background-color: transparent;
+    border: none;
+
+    display: none;
+    z-index: 1;
+
+    @media (max-width: 1024px) {
+      display: flex;
+    }
+
+    .stripe {
+      width: 100%;
+      height: 3px;
+
+      background-color: ${({ theme, hamburgerActive }) =>
+        hamburgerActive ? theme.colors.black : theme.colors.white};
+      border-radius: 1.5px;
+
+      transition: background-color 0.5s, transform 0.5s;
+      transform-origin: left center;
+
+      &:nth-child(2) {
+        opacity: ${({ hamburgerActive }) => (hamburgerActive ? '0' : '100%')};
+      }
+
+      &:nth-child(1) {
+        transform: ${({ hamburgerActive }) =>
+          hamburgerActive ? 'rotate(45deg) translateY(-3px)' : 'none'};
+      }
+
+      &:nth-child(3) {
+        transform: ${({ hamburgerActive }) =>
+          hamburgerActive ? 'rotate(-45deg) translateY(3px)' : 'none'};
+      }
+    }
+  }
+
+  .mobile-navigation {
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    transform: ${({ hamburgerActive }) =>
+      hamburgerActive ? 'translateX(0)' : 'translateX(-100%)'};
+
+    background-color: ${({ theme }) => theme.colors.white};
+
+    transition: transform 0.5s;
+
+    li {
+      padding: 15px;
+
+      font-size: ${({ theme }) => theme.fontSizes.S};
+      color: ${({ theme }) => theme.colors.black};
+
+      list-style: none;
+      cursor: pointer;
     }
   }
 `;
